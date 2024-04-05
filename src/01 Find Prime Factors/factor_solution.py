527a1b1b08bf47083e7870a2e4697c3ab2325946
Marina Zhigalova <42439962+marinazhigalova@users.noreply.github.com>
2024-04-05 16:28:23 +0000

    factor, palindrome

---
+++ src/01 Find Prime Factors/factor_solution.py
@@ -0,0 +1,18 @@
+def get_prime_factors(number):
+  factors = []
+  divisor = 2 # initial value
+  
+  while divisor <= number:
+    if number % divisor == 0:
+      factors.append(divisor)
+      number = number // divisor
+    else:
+      divisor += 1
+
+  return factors
+
+
+# commands used in solution video for reference
+if __name__ == '__main__':
+    print(get_prime_factors(1000))  # [2, 3, 3, 5, 7]
+    print(get_prime_factors(245))  # [13]
