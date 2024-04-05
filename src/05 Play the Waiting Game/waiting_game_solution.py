a55f1c0f8ab4c577123fd37b938063bb08e4b48c
Marina Zhigalova <42439962+marinazhigalova@users.noreply.github.com>
2024-04-05 18:23:09 +0000

    waiting game

---
+++ src/05 Play the Waiting Game/waiting_game_solution.py
@@ -0,0 +1,28 @@
+import random, time
+
+def waiting_game():
+    target_sec = random.randint(2,4)
+    print(f'You need to wait for exactly {target_sec} seconds to win a game!')
+
+    input('---Press ENTER to start---')
+    start_time = time.time()
+    
+    input('---Press ENTER to finish---')
+    end_time = time.time()
+
+    elapsed_time = end_time - start_time
+
+    if(elapsed_time == target_sec):
+        print(f'You WIN the game! You waited for exactly {elapsed_time:.3f} seconds!')
+    elif(elapsed_time > target_sec):
+        print(f'You waited for too long! {elapsed_time - target_sec :.3f} seconds more than needed')
+    elif(elapsed_time < target_sec): 
+        print(f'You did not waited long enough! {target_sec - elapsed_time :.3f} seconds less than needed')
+
+# commands used in solution video for reference
+if __name__ == '__main__':
+    waiting_game()
+
+
+
+
